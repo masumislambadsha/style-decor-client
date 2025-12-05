4540f87aad81b19d8625de082a12bcc6fec8ed59
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-12-06 03:42:56 +0600

    added admin route

---
+++ src/routes/AdminRoute.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import useAuth from "../Hooks/useAuth";
+import useRole from "../Hooks/useRole";
+import LoadingSpinner from "../Components/Spinner/LoadingSpinner";
+import Forbidden from "../Components/ForbiddenPage/Forbidden";
+
+const AdminRoute = ({ children }) => {
+  const { loading } = useAuth();
+  const { role, roleLoading } = useRole();
+  if (loading || roleLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (role !== "admin") {
+    return <Forbidden />;
+  }
+  return children;
+};
+
+export default AdminRoute;
