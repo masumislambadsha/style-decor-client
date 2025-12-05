0c5cb48848abf93c837f4ae3c5e82f63737b3b0b
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-12-06 00:28:19 +0600

    added essestian files and firebase secret in env

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClkLr1clo3L_rITzd0unX82QWPAe8TJSs",
+  authDomain: "style-decor-5b2fb.firebaseapp.com",
+  projectId: "style-decor-5b2fb",
+  storageBucket: "style-decor-5b2fb.firebasestorage.app",
+  messagingSenderId: "753772524948",
+  appId: "1:753772524948:web:3d181d7b3d3720de4dcb81"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
