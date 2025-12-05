88e816524f877a05a7b95decea1bdd2c79d891a9
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-12-06 03:50:57 +0600

    updated main.jsx and wraped with provides

--- src/Components/ForbiddenPage/Forbidden.jsx
+++ src/Components/ForbiddenPage/Forbidden.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Forbidden = () => {
   return (
     <div>
-
+      forbiidden
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,33 @@
+// src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+
 import { RouterProvider } from "react-router";
 import { router } from "./routes/Router.jsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./Context/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            duration: 2000,
+            style: {
+              fontFamily: "inherit",
+              borderRadius: "12px",
+            },
+          }}
+        />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
